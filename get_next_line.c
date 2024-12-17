453c20953546e4962917d4099f5953a00b9bf05b
Anass Rokhsi <arokhsi@e1r3p3.1337.ma>
2024-12-17 16:22:31 +0100

    auto save hh

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,56 @@
 /*   By: arokhsi <arokhsi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 10:43:00 by arokhsi           #+#    #+#             */
-/*   Updated: 2024/12/16 20:02:15 by arokhsi          ###   ########.fr       */
+/*   Updated: 2024/12/17 16:21:59 by arokhsi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static char	*ft_read(int fd)
-{
-	char	*tmp;
-	char	*buffer;
-	int		b_read;
-
-	tmp
-}
 
 static char	*ft_update_data()
 {
 	
 }
 
-static char	*ft_get_line()
+static char	*ft_get_line(char *data)
 {
-	
+	int	i;
+	int j;
+	char	*buffer;
+
+	if (!data || *data == '\0')
+		return (NULL);
+	i = 0;
+	while (data[i] != '\0' && data[i] != '\n')
+		i++;
+	buffer = malloc(i + 2);
+	if (!buffer)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		buffer[j] = data[j];
+		j++;
+	}
+	if (data[i] == '\n')
+		buffer[j++] = '\n';
+	buffer[j] = '\0';
+	return (buffer);
 }
 
 static char	*ft_get_data(int fd, char *pre_data)
 {
 	char	*buffer;
+	char	*data;
 
 	if (!pre_data)
 		return (ft_empty_string(pre_data));
+	if (n_l_check(pre_data))
+		return (pre_data);
 	buffer = ft_read(fd);
+	data = strljoin(pre_data, buffer, ft_strlen(buffer));
+	free(buffer);
+	return (data);
 }
 
 char	*get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,36 @@
 /*   By: arokhsi <arokhsi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 12:16:59 by arokhsi           #+#    #+#             */
-/*   Updated: 2024/12/16 19:24:53 by arokhsi          ###   ########.fr       */
+/*   Updated: 2024/12/17 16:21:59 by arokhsi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_read(int fd)
+{
+	char	*tmp;
+	char	*buffer;
+	int		b_read;
+
+	buffer = ft_empty_string(NULL);
+	tmp = malloc[BUFFER_SIZE + 1];
+	if (!tmp)
+		return (NULL);
+	tmp[BUFFER_SIZE] = '\0';
+	b_read = read(fd, tmp, BUFFER_SIZE);
+	while (b_read > 0)
+	{
+		buffer = strljoin(buffer, tmp, b_read);
+		if (n_l_check(tmp))
+			break;
+		b_read = read(fd, tmp, BUFFER_SIZE);
+	}
+	free(tmp)
+	if (read_byte <= 0 && !buffer)
+		return (NULL);
+	return (buffer);
+}
 
 char	*ft_empty_string(char *data)
 {
@@ -23,4 +48,81 @@ char	*ft_empty_string(char *data)
 		return (NULL);
 	buffer[0] = '\0';
 	return (buffer);
+}
+char	*ft_strljoin(char *s1, char *s2, int size)
+{
+	char	*buffer;
+	int		len;
+
+	len = ft_strlen(s1);
+	buffer = malloc(len + size + 1);
+	if (!buffer || !s2)
+		return (NULL);
+	if (!s1)
+		s1 = ft_init(NULL);
+	ft_strlcat("", s1, len);
+	ft_strlcat(buffer, s2, size);
+	free(s1);
+	return (buffer);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	if (!str)
+		return (0);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+int	n_l_check(char	*data)
+{
+	if (!data)
+		return (0);
+	while (*data)
+	{
+		if (*data == '\n')
+			return (1);
+		data++;
+	}
+	return (0);
+}
+char	*ft_strdup(char *str)
+{
+	char	*buffer;
+	int		i;
+
+	if (!str)
+		return (NULL);
+	buffer = malloc(ft_strlen(str) + 1);
+	if (!buffer)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen)
+	{
+		buffer[i] = str[i];
+		i++;
+	}
+	buffer[i] = '\0';
+	return (buffer);
+}
+char	*ft_strlcat(char *dest, const char *src, size_t size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
